d20e4dc9f3ac48558882f85d66c38c53d1717995
Nathan West <17nwest@coombedean.co.uk>
2023-07-10 13:29:36 +0100

    tezt commit

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,11 +1,20 @@
 import tkinter as tk
 root = tk.Tk()
+root.geometry("600x400")
 
 canvas1 = tk.Canvas(root, width=400, height=300)
 canvas1.pack()
 entry1 = tk.Entry(root) 
-canvas1.create_window(100, 40, window=entry1)
-entry1 = tk.Entry(root) 
-canvas1.create_window(100, 140, window=entry1)
+canvas1.create_window(300, 140, window=entry1)
+entry2 = tk.Entry(root) 
+canvas1.create_window(100, 140, window=entry2)
+
+request = tk.Button(root, text = "Request", bd = 5, command = root.destroy)
+
+request.place(x=200, y=50)
+
+
+
 
 root.mainloop()
+
