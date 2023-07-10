ce47f43e93135eb597a3f57f729560abb90a999d
Phoebe Say <phoebelou16@gmail.com>
2023-07-10 15:06:54 +0100

    progress

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,20 +1,34 @@
 import tkinter as tk
-root = tk.Tk()
-root.geometry("600x400")
+import requests
 
+root = tk.Tk()
+#set screeen
+root.geometry('600x400')
+root.title('Weather app')
+
+#text input
+lon_label = tk.Label(root, text = 'Longitude', font=('calibre',10, 'bold'))
 canvas1 = tk.Canvas(root, width=400, height=300)
 canvas1.pack()
 entry1 = tk.Entry(root) 
-canvas1.create_window(300, 140, window=entry1)
+canvas1.create_window(200, 100, window=entry1)
+
+lat_label = tk.Label(root, text = 'Latitude', font=('calibre',10, 'bold'))
+canvas2 = tk.Canvas(root, width=400, height=300)
+canvas2.pack()
 entry2 = tk.Entry(root) 
-canvas1.create_window(100, 140, window=entry2)
+canvas2.create_window(200, 200, window=entry1)
 
-request = tk.Button(root, text = "Request", bd = 5, command = root.destroy)
+#function for api
+def submit():
+    lat = entry1.get()
+    lon = entry2.get()
+    data = requests.get('https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=74109a70b2f459c87c25409811f4e6be'.format(lat,lon))
+    print(data)
 
-request.place(x=200, y=50)
-
-
+#button
+request = tk.Button(root, text = "Request", bd = 5, command = submit())
+request.place(x = 200, y = 20)
 
 
 root.mainloop()
-
