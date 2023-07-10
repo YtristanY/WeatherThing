155d53c7627177ec2a85f96339407de2eee07fc1
Phoebe Say <phoebelou16@gmail.com>
2023-07-10 13:34:49 +0100

    test

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 root = tk.Tk()
 
+#text input
 canvas1 = tk.Canvas(root, width=400, height=300)
 canvas1.pack()
 entry1 = tk.Entry(root) 
@@ -8,4 +9,11 @@ canvas1.create_window(100, 40, window=entry1)
 entry1 = tk.Entry(root) 
 canvas1.create_window(100, 140, window=entry1)
 
+#button
+request = tk.Button(root, text = "Request", bd = 5, command = root.destroy)
+
+request.pack(side = 'top')
+
 root.mainloop()
+
+hello
