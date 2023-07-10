999cfaa52027bb454574ac37f0a2edc81eb7e110
Nathan West <17nwest@coombedean.co.uk>
2023-07-10 12:49:33 +0100

    input box trial

---
+++ WeatherApp.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+root = tk.Tk()
+
+canvas1 = tk.Canvas(root, width=400, height=300)
+canvas1.pack()
+entry1 = tk.Entry(root) 
+canvas1.create_window(100, 40, window=entry1)
+entry1 = tk.Entry(root) 
+canvas1.create_window(100, 140, window=entry1)
+
+root.mainloop()
